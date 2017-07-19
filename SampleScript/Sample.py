201f3640a98d42b632dba50251af6e6a6597230c
Josh <joshayoung125@gmail.com>
2017-07-19 14:09:09 -0400

    Added live video capture that streams out yellow and light green images... Read comments in Sample.py.

--- SampleScript/Sample.py
+++ SampleScript/Sample.py
@@ -1,10 +1,34 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('SampleInput.jpg')
+# Gimp uses H = 0-360, S = 0-100 and V = 0-100. But OpenCV uses  H: 0 - 180, S: 0 - 255, V: 0 - 255
 
-ORANGE_MIN = np.array([5, 50, 50],np.uint8)
-ORANGE_MAX = np.array([15, 255, 255],np.uint8)
+#live image capture filtering our yellow stuff and light green for some reason... Still looking into hsv color ranges kinda just trial and error until it worked...
+cap = cv2.VideoCapture(0)
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    hsv_cap = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    YELLOW_MIN = np.array([20,175,75],np.uint8)
+    YELLOW_MAX = np.array([75,255,255],np.uint8);
+
+    cap_treshhold = cv2.inRange(hsv_cap, YELLOW_MIN, YELLOW_MAX)
+    cv2.imshow('frame', cap_treshhold)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+#code for changing that one sample image
+img = cv2.imread('SampleInput.jpg')
+hLow = 5
+sLow = 50
+vLow = 50
+hHigh = 15
+sHigh = 255
+vHigh = 255
+
+ORANGE_MIN = np.array([hLow, sLow, vLow],np.uint8)
+ORANGE_MAX = np.array([hHigh, sHigh, vHigh],np.uint8)
 
 hsv_img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
 
